f0048fc0a7e0524b5a9b14cbee4770b102e06cc4
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-03-06 13:39:37 +0300

    Add files via upload

---
+++ Graphs_labwork4.cpp
@@ -0,0 +1,33 @@
+﻿#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include "BMPGenerator.h"
+#include "FileReader.h"
+
+template<typename T>
+std::string to_string(T value) {
+    std::ostringstream os;
+    os << value;
+    return os.str();
+}
+
+int main() {
+    int numVertices, width, height;
+    readInputFromFile("graphs_data.txt", numVertices, width, height);
+
+    std::vector<Vertex> vertices;
+    std::vector<Edge> edges;
+
+    srand(time(NULL));
+
+    for (int i = 0; i < numVertices; ++i) {
+        vertices.push_back({ rand() % width, rand() % height, to_string(i) });
+    }
+
+    readEdgesFromFile("edge_data.txt", edges);
+
+    BMPGenerator bmpGenerator(width, height, vertices, edges);
+    bmpGenerator.generate("graph.bmp");
+
+    return 0;
+}
