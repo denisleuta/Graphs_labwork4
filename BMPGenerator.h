5a93d18a099bddfb6e674058fea12eca543f47ea
denisleuta <146765040+denisleuta@users.noreply.github.com>
2024-03-26 17:35:28 +0300

    Update BMPGenerator.h

--- BMPGenerator.h
+++ BMPGenerator.h
@@ -6,14 +6,20 @@
 #include <ctime>
 #include <cstdlib>
 #include <sstream>
+#include <set>
 #include "Vertex.h"
 #include "Edge.h"
 
 class BMPGenerator {
 public:
     BMPGenerator(int width, int height, const std::vector<Vertex>& vertices, const std::vector<Edge>& edges);
-
+    bool isGraphPlanar() const;
     void generate(const std::string& filename);
+    bool hasEdgeBetween(size_t v1, size_t v2) const;
+    bool containsK5() const;
+    bool containsK33() const;
+    void modifyForK5();
+    void modifyForK33();
 
 private:
     void writeHeader(std::ofstream& file);
@@ -30,4 +36,6 @@ private:
     int m_height;
     std::vector<Vertex> m_vertices;
     std::vector<Edge> m_edges;
+    std::vector<size_t> findK5Vertices() const;
+    std::vector<std::pair<size_t, size_t>> findK33Edges() const;
 };
